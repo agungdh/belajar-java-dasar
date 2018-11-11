dce9cc60dde17bf69c21406152f23686576b2cbb
agungdh <agungdh@live.com>
2018-11-11 14:49:38 +0700

    update

--- Duck.java
+++ Duck.java
@@ -1,5 +1,14 @@
 public class Duck {
-	public Duck() {
+	private int size;
+
+	public Duck(int size) {
+		// System.out.println(this.size);
+		// System.out.println(size);
+
 		System.out.println("Quack");
+
+		this.size = size;
+
+		System.out.println("Size is " + this.size);
 	}
 }
